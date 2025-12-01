ed5b77c18663a981a8df3a0def5984b4c53f07fb
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2025-12-01 13:50:32 +0000

    day one

--- src/main/kotlin/day01/day01.kt
+++ src/main/kotlin/day01/day01.kt
@@ -1,18 +1,30 @@
 package day01
 
-data class Rotation(val qty:Int, val direction:String)
+data class Rotation(val qty:Int, val direction: Direction)
+
+enum class Direction{Left, Right}
+
+fun String.toDirection() = if (this == "L") Direction.Left else Direction.Right
 
 data class  Safe(val position:Int, val noOfZeros:Int, val part:String = "one", val dialSize:Int = 100) {
-    fun rotate(qty:Int, direction:String) =
-        if (direction == "R") rotateRight(qty).newSafe(qty, direction) else rotateLeft(qty).newSafe(qty, direction)
 
-    fun rotateRight(qty:Int) = (position + qty) % 100
-    fun rotateLeft(qty:Int) = ((position + dialSize - qty % 100) % 100)
+    fun rotate(qty:Int, direction: Direction) = rotators.getValue(direction)(qty).newSafe(qty, direction)
 
-    fun Int.newSafe(qty:Int, direction:String) =
-        if (part == "one") Safe(this, if (this == 0) noOfZeros + 1 else noOfZeros)
-        else if (direction == "R") Safe(this, noOfZeros + (position + qty)/dialSize,part)
-        else Safe(this, noOfZeros + ((if (position > 0) (position - dialSize) else 0) - qty)/-dialSize,part)
+    val rotators = mapOf(
+        Direction.Right to {qty:Int -> (position + qty) % 100},
+        Direction.Left to {qty:Int ->((position + dialSize - qty % 100) % 100)}
+    )
+
+    fun Int.newSafe(qty:Int, direction: Direction) =
+        if (part == "one") Safe(this, updateZeroCountPartOne(newPosition = this))
+        else if (direction == Direction.Right) Safe(this, updateZeroCountPartTwoRight(qty), part)
+        else Safe(this, updateZeroCountPartTwoLeft(qty),part)
+
+    fun updateZeroCountPartOne(newPosition:Int)  = if (newPosition == 0) noOfZeros + 1 else noOfZeros
+
+    fun updateZeroCountPartTwoRight(qty:Int) = noOfZeros + (position + qty)/dialSize
+
+    fun updateZeroCountPartTwoLeft(qty:Int) = noOfZeros + ((if (position > 0) (position - dialSize) else 0) - qty)/-dialSize
 }
 
 fun day01(input:List<String>,part:String = "one") =
@@ -20,4 +32,4 @@ fun day01(input:List<String>,part:String = "one") =
         s.rotate(rotation.qty,rotation.direction)
     }
 
-fun String.toRotation() = Rotation(drop(1).toInt(),take(1))
+fun String.toRotation() = Rotation(drop(1).toInt(),take(1).toDirection())
