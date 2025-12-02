b86518c279920fc18a4d9dbc03d0831b6625c12f
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2025-12-02 08:36:39 +0000

    day one

--- src/main/kotlin/day01/day01.kt
+++ src/main/kotlin/day01/day01.kt
@@ -8,25 +8,25 @@ fun String.toDirection() = if (this == "L") Direction.Left else Direction.Right
 
 data class  Dial(val position:Int, val noOfZeros:Int, val part:String = "one", val dialSize:Int = 100) {
 
-    fun rotate(qty:Int, direction: Direction) = rotators.getValue(direction)(qty).newSafe(qty, direction)
+    fun rotate(qty:Int, direction: Direction) = rotators.getValue(direction)(qty).newDial(qty, direction)
 
     val rotators = mapOf(
         Direction.Right to {qty:Int -> (position + qty) % 100},
         Direction.Left to {qty:Int ->((position + dialSize - qty % 100) % 100)}
     )
 
-    fun Int.newSafe(qty:Int, direction: Direction)= copy(position = this, noOfZeros + zeroCount(this, qty,direction))
+    fun Int.newDial(qty:Int, direction: Direction)= copy(position = this, noOfZeros + zeroCount(this, qty,direction))
 
     fun zeroCount(newPosition:Int, qty:Int, direction: Direction) =
-        if (part == "one") updateZeroCountPartOne(newPosition)
-        else if (direction == Direction.Right) updateZeroCountPartTwoRight(qty)
-        else updateZeroCountPartTwoLeft(qty)
+        if (part == "one") calcZeroCountPartOne(newPosition)
+        else if (direction == Direction.Right) calcZeroCountPartTwoRight(qty)
+        else calcZeroCountPartTwoLeft(qty)
 
-    fun updateZeroCountPartOne(newPosition:Int)  = if (newPosition == 0)  1 else 0
+    fun calcZeroCountPartOne(newPosition:Int)  = if (newPosition == 0)  1 else 0
 
-    fun updateZeroCountPartTwoRight(qty:Int) = (position + qty)/dialSize
+    fun calcZeroCountPartTwoRight(qty:Int) = (position + qty)/dialSize
 
-    fun updateZeroCountPartTwoLeft(qty:Int) = ((if (position > 0) (position - dialSize) else 0) - qty)/-dialSize
+    fun calcZeroCountPartTwoLeft(qty:Int) = ((if (position > 0) (position - dialSize) else 0) - qty)/-dialSize
 }
 
 fun day01(input:List<String>,part:String = "one") =
