5bdbd91dfdd444c4eab79f4999f82d4526d34ca2
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2025-12-03 10:22:02 +0000

    day 3

--- src/main/kotlin/day03/day3.kt
+++ src/main/kotlin/day03/day3.kt
@@ -6,7 +6,7 @@ fun String.parse() = map{it.digitToInt()}
 
 fun partTwo(input:List<String>) = input.sumOf{ it.parse().maxSequence(digits = 12)}
 
-fun List<Int>.maxSequence(total:Long = 0, digits:Int = 2):Long {
+tailrec fun List<Int>.maxSequence(total:Long = 0, digits:Int = 2):Long {
     if (digits == 0) return total
     val max = dropLast(digits - 1).max()
     return drop(indexOf(max) + 1).maxSequence(total * 10L + max, digits -1 )
