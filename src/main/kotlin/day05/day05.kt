4ef74fc58c5b32dacfd1031bcfd4859c13d1d80a
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2025-12-08 09:41:29 +0000

    day05

--- src/main/kotlin/day05/day05.kt
+++ src/main/kotlin/day05/day05.kt
@@ -1,2 +1,35 @@
 package day05
 
+fun partOne(inputData:String):Int {
+    val (freshRanges, ingredients) = parse(inputData)
+    return freshIngredients(freshRanges, ingredients).size
+}
+
+fun freshIngredients(freshRanges:List<LongRange>, ingredients:List<Long>) =
+    ingredients.filter{ ingredient -> freshRanges.any{ingredient in it}}
+
+fun parse(inputData:String) = Pair(
+    inputData.split("\n\n")[0].split("\n").map{it.split("-")[0].toLong()..it.split("-")[1].toLong()},
+    inputData.split("\n\n")[1].split("\n").map(String::toLong)
+)
+
+fun partTwo(inputData: String):Long {
+    val (freshRanges, _) = parse(inputData)
+    val normalisedRanges = freshRanges.sortedBy { it.first }.normalise()
+    return normalisedRanges.sumOf { it.last - it.first + 1L }
+}
+
+fun List<LongRange>.normalise(output:List<LongRange> = emptyList()):List<LongRange> {
+    if (isEmpty()) return output
+    val next = if (output.isEmpty()) listOf(first()) else output.last() + first()
+    return drop(1).normalise(output.dropLast(1) + next)
+}
+
+operator fun LongRange.plus(other:LongRange) = add(this, other)
+
+fun add(r1:LongRange, r2:LongRange) = when {
+    r1.first <= r2.first && r1.last >= r2.last -> listOf(r1)
+    r1.first == r2.first -> listOf(r2)
+    r1.first < r2.first && r1.last >= r2.first -> listOf(r1.first..r2.last)
+    else -> listOf(r1, r2)
+}
--- src/test/kotlin/day05/day05test.kt
+++ src/test/kotlin/day05/day05test.kt
@@ -1,6 +1,55 @@
 package day05
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 
-class day05test:StringSpec ({
+class day05test: StringSpec( {
+    "parse input data into ranges and ingredients" {
+        val (ranges, ingredients) = parse(sampledata)
+        ranges shouldBe listOf(
+            3L..5L,10L..14L,16L..20L,12L..18L
+        )
+        ingredients shouldBe listOf(
+            1L, 5L, 8L, 11L, 17L, 32L
+        )
+    }
+    "part one with sample data should be 3" {
+        partOne(sampledata) shouldBe 3
+    }
+    "part one with puzzle input should be " {
+        partOne(puzzleInput) shouldBe 520
+    }
+    "sample data ranges normalised" {
+        parse(sampledata).first.sortedBy { it.first }.normalise() shouldBe listOf(
+            3L..5L,
+            10L..20L
+        )
+    }
+    "1..5 + 1..5 should be 1..5" {
+        (1L..5L) + (1L..5L) shouldBe listOf(1L..5L)
+    }
+    "1..5 + 1..5 should be 1..10" {
+        (1L..5L) + (1L..10L) shouldBe listOf(1L..10L)
+    }
+    "1..5 + 1..10 should be 1..5" {
+        (1L..10L) + (1L..5L) shouldBe listOf(1L..10L)
+    }
+    "1..10 + 5..10 should be 1..10" {
+        (1L..10L) + (5L..10L) shouldBe listOf(1L..10L)
+    }
+    "1..10 + 5..7 should be 1..10" {
+        (1L..10L) + (5L..7L) shouldBe listOf(1L..10L)
+    }
+    "1..5 + 5..10 should be 1..10" {
+        (1L..5L) + (5L..10L) shouldBe listOf(1L..10L)
+    }
+    "1..4 + 5..10 should be 1..4, 5..10" {
+        (1L..4L) + (5L..10L) shouldBe listOf(1L..4L, 5L..10L)
+    }
+    "part two with sample data is 14" {
+        partTwo(sampledata) shouldBe 14L
+    }
+    "part two with puzzle input is 14" {
+        partTwo(puzzleInput) shouldBe 347338785050515L
+    }
 })
