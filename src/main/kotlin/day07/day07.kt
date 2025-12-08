231b5fe88ee186c72f66e37584de7ce34398b855
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2025-12-08 08:05:30 +0000

    day07

--- src/main/kotlin/day07/day07.kt
+++ src/main/kotlin/day07/day07.kt
@@ -1,2 +1,25 @@
 package day07
 
+data class Point(val row:Int, val col:Int) {
+    fun belowLeft() = Point(row + 1, col -1)
+    fun belowRight() = Point(row + 1, col + 1)
+}
+
+fun partTwo(input:List<String>):Long {
+    return input.routes(input.start())
+}
+
+fun List<String>.routes(point:Point, memory:MutableMap<Point, Long> = mutableMapOf()):Long {
+    if (point in memory) return memory.getValue(point)
+    val splitter = pointBelow(point)
+    return (if (splitter != null) {
+        routes(splitter.belowLeft(), memory) + routes(splitter.belowRight(), memory)
+    } else 1)
+        .also { memory[point] = it  }
+}
+
+fun List<String>.start() = Point(0,first().indexOf('S'))
+
+fun List<String>.pointBelow(point:Point):Point? =
+    ((point.row + 1)..lastIndex).firstOrNull{ this[it][point.col] == '^' }?.let{Point(it, point.col)}
+
--- src/test/kotlin/day07/day07test.kt
+++ src/test/kotlin/day07/day07test.kt
@@ -1,7 +1,16 @@
 package day07
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 
 class day07test:StringSpec ({
-
+    "point below start is point()" {
+        sampledata.pointBelow(sampledata.start()) shouldBe Point(2,7)
+    }
+    "part two with sampl data is 40" {
+        partTwo(sampledata) shouldBe 40L
+    }
+    "part two with puzzle input is 108924003331749L" {
+        partTwo(puzzleinput) shouldBe 108924003331749L
+    }
 })
