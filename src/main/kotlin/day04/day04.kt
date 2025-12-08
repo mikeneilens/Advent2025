88c4bcfb74a81334c5e6b0fd96464875457a785e
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2025-12-08 09:37:35 +0000

    day04

--- src/main/kotlin/day04/day04.kt
+++ src/main/kotlin/day04/day04.kt
@@ -1,2 +1,35 @@
 package day04
 
+fun partOne(inputData:List<String>):Int =
+    removeRolls(inputData).sumOf { line ->  line.count { it == 'X' } }
+
+fun List<String>.square(point:Point) = this[point.row][point.col]
+
+fun List<String>.surroundingSquares(point:Point) = point.surroundingPoints().map{this.square(it)}
+
+data class Point(val row:Int, val col:Int, val rowRange:IntRange = 0..9, val colRange:IntRange = 0..9) {
+    fun surroundingPoints() =
+        (-1..1).flatMap{ r -> (-1..1).mapNotNull { c -> if ((c==0 && r==0) || row + r !in rowRange  || col+c !in colRange) null else Point(row + r, col + c ) } }
+}
+
+fun partTwo(inputData:List<String>):Int {
+    return keepRemovingRolls(inputData).sumOf { line ->  line.count { it == 'X' } }
+}
+
+fun keepRemovingRolls(grid:List<String>):List<String> {
+    val newGrid = removeRolls(grid)
+    return if (newGrid == grid) newGrid else keepRemovingRolls(newGrid)
+}
+
+fun removeRolls(grid:List<String>):List<String> {
+    val rowRange = 0..grid.lastIndex
+    val colRange = 0..grid.first().lastIndex
+    return rowRange.map{row ->
+        val columns = colRange.mapNotNull{col ->
+            if (grid.square(Point(row, col)) == '@' && grid.surroundingSquares(Point(row, col, rowRange, colRange)).count { it =='@' } < 4)
+                'X'
+            else grid[row][col]
+        }
+        columns.joinToString("")
+    }
+}
--- src/test/kotlin/day04/day04test.kt
+++ src/test/kotlin/day04/day04test.kt
@@ -1,6 +1,77 @@
 package day04
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 
-class day04test:StringSpec ({
+class day04test: StringSpec ({
+    "points surrounding point (5,5) are (4,4),(4,5),(4,6), (5,4),(5,6),(6,4),(6,5),(6,6) " {
+        Point(row=5,col=5).surroundingPoints().toSet() shouldBe listOf(
+            Point(4,4),
+            Point(4,5),
+            Point(4,6),
+            Point(5,4),
+            Point(5,6),
+            Point(6,4),
+            Point(6,5),
+            Point(6,6),
+        ).toSet()
+    }
+    "points surrounding point(0,5) are (0,4) (0,6), (1,4),(1,5),(1,6)" {
+        Point(row=0,col=5).surroundingPoints().toSet() shouldBe listOf(
+            Point(0,4),
+            Point(0,6),
+            Point(1,4),
+            Point(1,5),
+            Point(1,6),
+        ).toSet()
+    }
+    "points surrounding point(9,5) are (9,4) (9,6), (8,4),(8,5),(8,6)" {
+        Point(row = 9, col = 5).surroundingPoints().toSet() shouldBe listOf(
+            Point(9, 4),
+            Point(9, 6),
+            Point(8, 4),
+            Point(8, 5),
+            Point(8, 6),
+        ).toSet()
+    }
+    "sample data has 13 rolls of paper accessible" {
+        partOne(sampledata) shouldBe 13
+    }
+    "puzzle data has rolls of paper accessible" {
+        partOne(puzzleinput) shouldBe 1449
+    }
+    "delete all rolls from sample data once" {
+        removeRolls(sampledata) shouldBe listOf(
+            "..XX.XX@X.",
+            "X@@.@.@.@@",
+            "@@@@@.X.@@",
+            "@.@@@@..@.",
+            "X@.@@@@.@X",
+            ".@@@@@@@.@",
+            ".@.@.@.@@@",
+            "X.@@@.@@@@",
+            ".@@@@@@@@.",
+            "X.X.@@@.X."
+        )
+    }
+    "keep removing rolls from sample data" {
+        keepRemovingRolls(sampledata) shouldBe listOf(
+            "..XX.XXXX.",
+            "XXX.X.X.XX",
+            "XXXXX.X.XX",
+            "X.XX@@..X.",
+            "XX.@@@@.XX",
+            ".XX@@@@@.X",
+            ".X.@.@.@@X",
+            "X.X@@.@@@X",
+            ".XX@@@@@X.",
+            "X.X.@@@.X."
+        )
+    }
+    "part two with sample data is 43" {
+        partTwo(sampledata) shouldBe 43
+    }
+    "part two with puzzleInput is 8746" {
+        partTwo(puzzleinput) shouldBe 8746
+    }
 })
