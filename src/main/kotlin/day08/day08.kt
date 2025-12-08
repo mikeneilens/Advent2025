6d1d106b07afe4e23897c884c3e7c31af2456ed3
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2025-12-08 18:03:22 +0000

    day 8 not working

--- src/main/kotlin/day08/day08.kt
+++ src/main/kotlin/day08/day08.kt
@@ -1,4 +1,49 @@
 package day08
 
-class day08 {
+import kotlin.math.sqrt
+
+fun partOne(input:List<String>, size:Int = 10):Long {
+    val closestToEachPoint = input.toPoints().closestToEach().take(size)
+    val circuits = makeCircuits(closestToEachPoint)
+    return circuits.sortedByDescending{it.size}.take(3).fold(1L) { a, j -> a * j.size }
 }
+
+data class Point(val x:Int, val y:Int, val z:Int) {
+    operator fun minus(other:Point) = Point(x - other.x, y - other.y, z - other.z)
+    val distance: Double get() = sqrt(((x * x) + (y * y) + (z * z)).toDouble())
+}
+
+fun List<String>.toPoints() = map{ Point(it.split(",")[0].toInt(),it.split(",")[1].toInt(),it.split(",")[2].toInt()) }
+
+fun makeCircuits(closestPoints:List<Pair<Point, Point>>, circuits:MutableList<MutableSet<Point>> = mutableListOf()):MutableList<MutableSet<Point>> {
+    closestPoints.forEach { (p1,p2) ->
+        val p1Circuits = circuits.find { circuit ->  p1 in circuit }
+        val p2Circuits = circuits.find { circuit ->  p2 in circuit }
+        when {
+            p1Circuits == null && p2Circuits == null -> circuits.add(mutableSetOf(p1, p2))
+            p1Circuits == null -> p2Circuits?.add(p1)
+            p2Circuits == null -> p1Circuits?.add(p2)
+            else -> {
+                circuits.remove(p1Circuits)
+                circuits.remove(p2Circuits)
+                circuits.add((p1Circuits + p2Circuits).toMutableSet())
+            }
+        }
+    }
+    return circuits
+}
+
+fun List<Point>.closestToEach() = getPairs().map { Pair(it, distanceBetween(it.first, it.second))}
+    .sortedBy { it.second }.map{it.first}
+
+fun List<Point>.getPairs(): List<Pair<Point, Point>> {
+    val output = mutableListOf<Pair<Point, Point>>()
+    forEachIndexed { i, first ->
+        drop(i + 1).forEach { second ->
+            output.add(Pair(first, second))
+        }
+    }
+    return output
+}
+
+fun distanceBetween(point1:Point, point2:Point) =  ( (point1 - point2).distance)
--- src/test/kotlin/day08/day08test.kt
+++ src/test/kotlin/day08/day08test.kt
@@ -1,7 +1,32 @@
 package day08
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 
 class day08test:StringSpec ({
-
+    "closest to each point" {
+        val closestToEachPoint = sampledata.toPoints().closestToEach()
+        closestToEachPoint[0] shouldBe setOf(Point(162,817,812), Point(425,690,689))
+        closestToEachPoint[1] shouldBe setOf(Point(x=162, y=817, z=812), Point(x=431, y=825, z=988))
+        closestToEachPoint[2] shouldBe setOf(Point(x=906, y=360, z=560), Point(x=805, y=96, z=715))
+        closestToEachPoint[3] shouldBe setOf(Point(x=431, y=825, z=988 ), Point(x=425,y=690,z=689))
+        closestToEachPoint[4] shouldBe setOf(Point(x=862, y=61, z=35), Point(x=984, y=92, z=344))
+        closestToEachPoint[5] shouldBe setOf(Point(x=52, y=470, z=668), Point(x=117, y=168, z=530))
+        closestToEachPoint[6] shouldBe setOf(Point(x=819, y=987, z=18), Point(x=941, y=993, z=340))
+        closestToEachPoint[7] shouldBe setOf(Point(x=906, y=360, z=560), Point(x=739, y=650, z=466))
+        closestToEachPoint[8] shouldBe setOf(Point(x=346, y=949, z=466), Point(x=425, y=690, z=689))
+        closestToEachPoint[9] shouldBe setOf(Point(x=906, y=360, z=560), Point(x=984, y=92, z=344))
+    }
+    "connect all points" {
+        val closestToEachPoint = sampledata.toPoints().closestToEach().take(10)
+        val circuits = makeCircuits(closestToEachPoint)
+        circuits.sortedByDescending{it.size}.take(3).fold(1) { a, j -> a * j.size } shouldBe 40
+    }
+    "part one with sample data" {
+        partOne(sampledata, size = 10) shouldBe 40L
+    }
+    "part one with puzzle input" {
+        //38171 is too low
+        partOne(puzzleinput, size = 1000) shouldBe 40
+    }
 })
