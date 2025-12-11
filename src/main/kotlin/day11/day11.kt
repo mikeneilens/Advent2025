91b5896842783f2ba6270215b98ecd60265037b6
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2025-12-11 13:25:47 +0000

    day 11

--- src/main/kotlin/day11/day11.kt
+++ src/main/kotlin/day11/day11.kt
@@ -5,14 +5,13 @@ fun partOne(input:List<String>) =  input.routes("you")
 
 fun String.outputDevices() = split(": ")[1].split(" ")
 
-fun List<String>.routes(device:String, end:String = "out", memory:MutableMap<String, Long> = mutableMapOf()):Long {
-    if (device in memory) return memory.getValue(device)
-    if (device == end) return 1
-    val outputDevices = firstOrNull() {it.startsWith(device)}?.outputDevices() ?: listOf()
-    return (outputDevices.sumOf{ outputDevice ->
-        routes(outputDevice, end, memory)
-    }).also{memory[device] = it}
-}
+fun List<String>.outputsFor(device:String) = firstOrNull{it.startsWith(device)}?.outputDevices() ?: listOf()
+
+fun List<String>.routes(device:String, end:String = "out", memory:MutableMap<String, Long> = mutableMapOf()):Long =when {
+        (device in memory) -> memory.getValue(device)
+        (device == end) -> 1L
+        else -> (outputsFor(device).sumOf{ outputDevice -> routes(outputDevice, end, memory) }).also{memory[device] = it}
+    }
 
 fun partTwo(input:List<String>) =
     input.routes(device = "svr", end = "fft") * input.routes("fft",end = "dac") * input.routes("dac",end = "out") +
