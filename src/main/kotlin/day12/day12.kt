7c794557aff1d877be0aeddc21538886b8e97142
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2025-12-12 14:05:25 +0000

    day 12

--- src/main/kotlin/day12/day12.kt
+++ src/main/kotlin/day12/day12.kt
@@ -1,2 +1,24 @@
 package day12
 
+fun partOne(input:List<String>):Int {
+    val regions = input.getRegions()
+    val presents = input.getPresentSizes()
+    return regions.count { region -> region.regionCouldContain(presents) }
+}
+
+fun List<String>.getPresentSizes() = dropLast(1).associate { it.split("\n").getPresentSize() }
+
+fun List<String>.getRegions() = last().split("\n").map{it.getRegion()}
+
+fun List<String>.getPresentSize() = Pair(
+        first().first(),
+        drop(1).sumOf{ line -> line.count{ it == '#' }}
+    )
+
+fun String.getRegion() = Pair(
+    split(":").first().split("x").fold(1){a,r -> a * r.toInt()},
+    split(": ").last().split(" ").map(String::toInt)
+)
+
+fun Pair<Int, List<Int>>.regionCouldContain(presents:Map<Char, Int>) =
+    second.mapIndexed { i, q -> presents.getValue(i.toString()[0]) * q}.sum() <= first
--- src/test/kotlin/day12/day12test.kt
+++ src/test/kotlin/day12/day12test.kt
@@ -1,6 +1,43 @@
 package day12
 
+import day02.partOne
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 
 class day12test:StringSpec ({
+    "convert first item of sampleData into a present" {
+        val (number, size) = sampledata.first().split("\n").getPresentSize()
+        number shouldBe '0'
+        size shouldBe 7
+    }
+    "convert first region in sample data" {
+        val (size, presentsRequired) = sampledata.last().split("\n").first().getRegion()
+        size shouldBe 16
+        presentsRequired shouldBe listOf(0, 0, 0, 0, 2, 0)
+    }
+    "get presents in sample data" {
+        sampledata.getPresentSizes() shouldBe mapOf(
+            '0' to 7 , '1' to 7, '2' to 7, '3' to 7, '4' to 7, '5' to 7
+        )
+    }
+    "get regions in sample data" {
+        sampledata.getRegions() shouldBe listOf(
+            Pair(16, listOf(0, 0, 0, 0, 2, 0)),
+            Pair(60, listOf(1, 0, 1, 0, 2, 2)),
+            Pair(60, listOf(1, 0, 1, 0, 3, 2))
+        )
+    }
+    "presents fit in region 0" {
+        val region = sampledata.getRegions()[0]
+        val presents = sampledata.getPresentSizes()
+        region.regionCouldContain(presents) shouldBe true
+    }
+    "presents dont fit in region 2" {
+        val region = sampledata.getRegions()[2]
+        val presents = sampledata.getPresentSizes()
+        region.regionCouldContain(presents) shouldBe true
+    }
+    "part one with puzzle input" {
+        partOne(puzzleinput) shouldBe 448
+    }
 })
